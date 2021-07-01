619bfb7297c3c95594a918062271399a6e026ea6
ravenera0317 <r6090408@gmail.com>
2021-07-01 23:29:04 +0800

    Change static show page to dynamic one with params method

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
-app.get('/restaurants/1', (req, res) => {
-  res.render('show')
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find((restaurant) => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
 })
 
 // start and listen on the Express server
