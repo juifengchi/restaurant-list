719a049233eabf23b494a19e0ece9a21eddb1f9d
ravenera0317 <r6090408@gmail.com>
2021-07-01 22:23:36 +0800

    Change static index page to dynamic one with JSON file

--- app.js
+++ app.js
@@ -2,9 +2,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-// require express-handlebars here
 const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -15,7 +14,7 @@ app.use(express.static('public'))
 
 // routes setting
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { restaurants: restaurantList.results })
 })
 
 // start and listen on the Express server
