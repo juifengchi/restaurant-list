75b787f0af7ef343f0e6f8ee958171fafc21ee31
ravenera0317 <r6090408@gmail.com>
2021-07-12 00:49:21 +0800

    Add search function

--- app.js
+++ app.js
@@ -106,11 +106,16 @@ app.post('/restaurants/:id/delete', (req, res) => {
     .catch(error => console.error(error))
 })
 
-// app.get('/search', (req, res) => {
-//   const keyword = req.query.keyword
-//   const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
-//   res.render('index', { restaurants, keyword })
-// })
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      restaurants = restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase()))
+      res.render('index', { restaurants, keyword })
+    })
+    .catch(error => console.error(error))
+})
 
 // start and listen on the Express server
 app.listen(port, () => {
