84d5964d4b4135f6afe9ade9cf2426061e49e01f
ravenera0317 <r6090408@gmail.com>
2021-07-11 18:13:19 +0800

    feat: modify restaurant seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -13,18 +13,18 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongodb connected!')
   for (let i = 0; i < restaurantList.results.length; i++) {
-    Restaurant.create(
-      { id: restaurantList.results[i].id },
-      { name: restaurantList.results[i].name },
-      { name_en: restaurantList.results[i].name_en },
-      { category: restaurantList.results[i].category },
-      { image: restaurantList.results[i].image },
-      { location: restaurantList.results[i].location },
-      { phone: restaurantList.results[i].phone },
-      { google_map: restaurantList.results[i].google_map },
-      { rating: restaurantList.results[i].rating },
-      { description: restaurantList.results[i].description }
-    )
+    Restaurant.create({
+      id: restaurantList.results[i].id,
+      name: restaurantList.results[i].name,
+      name_en: restaurantList.results[i].name_en,
+      category: restaurantList.results[i].category,
+      image: restaurantList.results[i].image,
+      location: restaurantList.results[i].location,
+      phone: restaurantList.results[i].phone,
+      google_map: restaurantList.results[i].google_map,
+      rating: restaurantList.results[i].rating,
+      description: restaurantList.results[i].description
+    })
   }
   console.log('done')
 })
