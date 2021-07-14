39dc95c9d1d28ab3e67fa8716c295b6cbdb141fc
ravenera0317 <r6090408@gmail.com>
2021-07-14 15:24:40 +0800

    refactor: modify mongoose config

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,17 +1,9 @@
 const restaurantList = require('./restaurant.json')
-const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
 
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('mongodb connected!')
   for (let i = 0; i < restaurantList.results.length; i++) {
     Restaurant.create({
       id: restaurantList.results[i].id,
